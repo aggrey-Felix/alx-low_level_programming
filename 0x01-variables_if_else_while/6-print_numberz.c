dc1e2450f6139c3006398bdb9132c2b1c23ecccc
felix abongo <aggreyfelix@yahoo.com>
2021-08-27 01:21:03 -0700

    printing all single digits in base 10 from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 0-9 without
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int k = '0';
+
+	while (k <= '9')
+	{
+		putchar(i);
+		k++;
+	}
+	putchar('\n');
+	return (0);
+}
