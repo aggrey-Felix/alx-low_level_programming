0f549651f379d3dcdbdfac3c49ea44bf63440635
felix abongo <aggreyfelix@yahoo.com>
2021-08-31 02:23:09 -0700

    printing holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,21 +6,12 @@
  * Description: uses the holbertong header file
  * that contains the function _putchar
  * Return: 0.
-*/
-
-
-int main(void)
-{
-	char str[] = "Holberton";
-	int i;
-
-	i = 0;
-	while (str[i])
-	{
-		_putchar(str[i]);
-		++i;
-	}
-	_putchar('\n');
-
-	return (0);
+ */
+int main(void) {
+  _putchar("Holberton\n");
+  return 0;
 }
+
+
+
+
